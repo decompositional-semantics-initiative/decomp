2f3c270e25c870a42fc632331a34841005395eeb
Aaron Steven White <aaron.white@rochester.edu>
2021-03-19 10:48:28 -0400

    Bumps version 0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Aaron Steven White'
 author = 'Aaron Steven White'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0a2'
+release = '0.2.0'
 
 # Changes root document from contents.rst to index.rst
 master_doc = 'index'
